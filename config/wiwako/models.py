78adaab8d208896bf6660be5e99508aa9a219971
Dav1d43 <158083435+Dav1d43@users.noreply.github.com>
2024-04-04 23:51:28 +0400

    modelebi

--- config/wiwako/models.py
+++ config/wiwako/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from categories.models import Category
 
-# Create your models here.
+
+class Wiwako(models.Model):
+    saxeli_qartulad = models.CharField(max_length=50)
+    saxeli_inglisurad = models.CharField(max_length=50)
+    agwera = models.TextField(null=True)
+    maragshia = models.BooleanField(default=True)
+    fasi = models.FloatField()
+    photo = models.ImageField(upload_to="wiwakebi/")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.saxeli_qartulad
+
