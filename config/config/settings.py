bbfe3e0c7c5e3e4353d0a535b0c48e240d8ebeeb
UuTTsU <158373418+UuTTsU@users.noreply.github.com>
2024-04-05 00:01:19 +0400

    admini, settingebi

--- config/config/settings.py
+++ config/config/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'carousel',
+    'categories',
+    'wiwako'
 ]
 
 MIDDLEWARE = [
@@ -116,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = BASE_DIR / "media/"
+MEDIA_URL = "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- config/wiwako/admin.py
+++ config/wiwako/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Category, Wiwako
+from carousel.models import CarouselItem
 
-# Register your models here.
+
+
+@admin.register(Wiwako)
+class WiwakoAdmin(admin.ModelAdmin):
+    list_display = ['saxeli_qartulad', 'saxeli_inglisurad', 'category', 'maragshia', 'fasi']
+    list_filter = ['category', 'maragshia']
+    search_fields = ['saxeli_qartulad', 'saxeli_inglisurad']
+    list_per_page = 20
+
+
+admin.site.register(Category)
+
+
+
+
+
+@admin.register(CarouselItem)
+class CarouselItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'wiwako', 'get_redirect_url')
+    readonly_fields = ('get_redirect_url',)
