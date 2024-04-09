0a78929b8dd584c1caae7de8ca67f36e77e45ded
Dav1d43 <158083435+Dav1d43@users.noreply.github.com>
2024-04-09 12:59:04 +0400

    Ganaxlebuli modeleb1

--- config/categories/models.py
+++ config/categories/models.py
@@ -1,15 +1,7 @@
-from typing import List, Tuple
 from django.db import models
 
 class Category(models.Model):
-    CATEGORY_CHOICES: List[Tuple[str, str]] = [
-        ('category1', 'Category1'),
-        ('category2', 'Category2'),
-        ('category3', 'Category3'),
-        ('pepper', 'Pepper'),
-        ('dwarf_tomatoes', 'Dwarf tomatoes'),
-        ('basilica', 'Basilica'),
-    ]
+    name = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
