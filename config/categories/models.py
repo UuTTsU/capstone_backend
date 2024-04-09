a347565717ddede8f0e8200b2ea52e03a3ceb6d6
Dav1d43 <158083435+Dav1d43@users.noreply.github.com>
2024-04-09 13:01:19 +0400

    Ganaxlebuli modeleb1

--- config/categories/models.py
+++ config/categories/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    CATEGORY_CHOICES = [
+        ('category1', 'Category1'),
+        ('category2', 'Category2'),
+        ('category3', 'Category3'),
+        ('pepper', 'Pepper'),
+        ('dwarf_tomatoes', 'Dwarf tomatoes'),
+        ('basilica', 'Basilica'),
+    ]
+
+    title = models.CharField(max_length=100, choices=CATEGORY_CHOICES, primary_key=True)
 
     def __str__(self):
-        return self.name
+        return self.title
